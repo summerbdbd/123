56cb3e30bf2172564fe603cdd1e7ddd8ae8ce60d
summerbdbd <64152012+summerbdbd@users.noreply.github.com>
2020-04-29 01:27:45 +0900

    WEEK6

--- todo/forms.py
+++ todo/forms.py
@@ -5,4 +5,4 @@ from .models import Todo
 class AddForm(forms.ModelForm):
     class Meta:
         model = Todo
-        fields = ('task',) #문장만듬, 네모만든다 생각하시면 됨
+        fields = ('task',) #문장만듬, 네모만든다 생각하시면 됨!
